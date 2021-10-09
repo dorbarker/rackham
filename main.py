b4fb1bd521bb552a840d07bbb6546471b2e1a15e
Dillon Barker <dor.barker@gmail.com>
2021-10-09 17:47:55 -0500

    add functions for gathering loci

--- main.py
+++ main.py
@@ -18,6 +18,34 @@ def arguments():
     parser.add_argument("pangenome", type=Path)
 
 
+def threshold_count_calculate(gffs_dir: Path, threshold: float) -> int:
+
+    n_genomes = len(list(gffs_dir.glob("*.gff")))
+
+    return round(threshold * n_genomes)
+
+
+def loci_over_threshold(gene_families: Path, threshold_count: int) -> set[str]:
+
+    gene_families = pd.read_csv(gene_families, sep=",")
+
+    over_threshold = gene_families["number_genomes"] > threshold_count
+
+    loci = gene_families["gene_family"].loc[over_threshold]
+
+    return set(loci)
+
+
+def paralogs_get(paralog_list: Path) -> set[str]:
+
+    return set(paralog_list.read_text().splitlines())
+
+
+def loci_paralogs_remove(loci, paralogs):
+
+    return loci - paralogs
+
+
 def main():
     pass
 
