6dbbf4e956ab728ea4ada36f6412ec13ec0500b2
Dillon Barker <dor.barker@gmail.com>
2021-10-10 17:05:03 -0500

    add setup.py and organize into module

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+from rackham import __version__
+
+setup(
+    name="rackham",
+    version=__version__,
+    packages=find_packages,
+    install_requires=["biopython", "pandas"],
+    author="Dillon O.R. Barker",
+    author_email="dillon.barker@phac-aspc.gc.ca",
+    entry_points={"console_scripts": ["rackham=rackham.rackham:main"]},
+)
