e07f3d6d1cd6f01965e643b1583e7ebd25f93244
Dillon Barker <dor.barker@gmail.com>
2021-10-10 16:53:43 -0500

    check for length variability

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import argparse
 from pathlib import Path
 from shutil import copyfile
+from Bio import SeqIO
+import statistics
 
 
 def arguments():
@@ -9,14 +11,31 @@ def arguments():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "--threshold",
+        "--carriage-threshold",
         "-t",
         type=float,
         default=1.0,
         help="Proportion of genomes a locus must be present in to be included",
     )
 
-    parser.add_argument("pangenome", type=Path)
+    parser.add_argument(
+        "--length-tolerance",
+        "-l",
+        type=float,
+        default=0.01,
+        help="Maximum allowed value for (min_length / max_length) for alleles of a given locus",
+    )
+    parser.add_argument(
+        "pangenome", type=Path, help="Pangenome directory created by PIRATE"
+    )
+
+    parser.add_argument(
+        "output", type=Path, help="Output directory containing cgmlst alleles"
+    )
+
+    args = parser.parse_args()
+
+    return args
 
 
 def threshold_count_calculate(gffs_dir: Path, threshold: float) -> int:
@@ -47,8 +66,21 @@ def loci_paralogs_remove(loci, paralogs):
     return loci - paralogs
 
 
+def is_length_variable(locus: Path, length_tolerance: float):
+
+    with locus.open("r") as f:
+        lengths = [len(rec.seq) for rec in SeqIO.parse(f, "fasta")]
+
+    minimum, median, maximum = min(lengths), statistics.median(lengths), max(lengths)
+
+    return maximum / minimum > length_tolerance
+
+
 def copy_alleles(
-    filtered_loci: set[str], sequence_directory: Path, allele_directory: Path
+    length_tolerance: float,
+    filtered_loci: set[str],
+    sequence_directory: Path,
+    allele_directory: Path,
 ) -> None:
 
     allele_directory.mkdir(parents=True, exist_ok=True)
@@ -57,19 +89,53 @@ def copy_alleles(
 
     for seq in feature_sequences:
 
-        if seq.replace(".", "_").split("_")[0] in filtered_loci:
+        if str(seq).replace(".", "_").split("_")[0] in filtered_loci:
 
-            dst = allele_directory.joinpath(seq.name)
+            if not is_length_variable(seq, length_tolerance):
+                dst = allele_directory.joinpath(seq.name)
 
-            copyfile(seq, dst)
+                copyfile(seq, dst)
 
-    # check that all is well
-    if len(list(allele_directory.glob("*.fasta"))) != len(filtered_loci):
-        raise IncompleteCopyError
+
+def filtered_loci_get(
+    carriage_threshold: float,
+    length_tolerance: float,
+    gffs_dir: Path,
+    paralog_clusters: Path,
+    gene_families: Path,
+    feature_sequences_directory: Path,
+    alleles_directory: Path,
+) -> None:
+
+    threshold_count = threshold_count_calculate(gffs_dir, carriage_threshold)
+
+    high_carriage = loci_over_threshold(gene_families, threshold_count)
+
+    paralogs = paralogs_get(paralog_clusters)
+
+    high_carriage_orthologs = loci_paralogs_remove(high_carriage, paralogs)
+
+    copy_alleles(
+        length_tolerance=length_tolerance,
+        filtered_loci=high_carriage_orthologs,
+        sequence_directory=feature_sequences_directory,
+        allele_directory=alleles_directory,
+    )
 
 
 def main():
-    pass
+
+    args = arguments()
+
+    filtered_loci_get(
+        carriage_threshold=args.carriage_threshold,
+        length_tolerance=args.length_tolerance,
+        gffs_dir=args.pangenome / "modified_gffs/",
+        paralog_clusters=args.pangenome / "paralog_clusters.tab",
+        gene_families=args.pangenome / "PIRATE.gene_families.tsv",
+        feature_sequences_directory=args.pangenome / "feature_sequences/",
+        alleles_directory=args.output,
+    )
 
 
 class IncompleteCopyError(Exception):
